b7cd05c334c42aa864ec39a15fd2d58468b5f81b
pgiardiniere <pgiardiniere@gmail.com>
2020-01-21 15:32:05 -0500

    get avg and sum, no input checks

---
+++ lab01-mathy/mathy.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int sum(int num) {
+    int sum = 0;
+    while (num > -1) {
+        sum += num;
+        num--;
+    }
+    return sum;
+}
+
+double avg(int num) {
+    double avg = 0;
+    int summed = sum(num);
+    avg = (double)summed/num;
+    return avg;
+}
+
+int main() {
+    printf("hello you should input an integer friend\n");
+    int thenum;
+    int summed;
+    double avged;
+
+    scanf("%d", &thenum);
+    summed = sum(thenum);
+    avged  = avg(thenum);
+
+    printf("the num is %d\n",  thenum);
+    printf("the sum is %d\n",  summed);
+    printf("the avg is %lf\n", avged);
+
+    return 0;
+}
