e4fcaca6d977351677c84ec81d2f15a7d1316cd3
pgiardiniere <pgiardiniere@gmail.com>
2020-01-24 00:54:32 -0500

    lab01 completed

--- lab01-mathy/mathy.c
+++ lab01-mathy/mathy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int sum(int num) {
     int sum = 0;
@@ -17,20 +18,26 @@ double avg(int num) {
 }
 
 int main() {
-    printf("hello you should input an integer friend\n");
-    int thenum;
+    printf("hello! you should input positive, non-zero integer, friend\n\n");
     int summed;
     double avged;
+    char str[256];
+    int thenum;
 
-    scanf("%d", &thenum);
+    scanf("%s", str);
+    thenum = atoi(str);    
+    while (thenum <= 0) {
+        printf("You seem poor at following instructions. Please input a positive, nonzero integer.\n");
+        printf("If you input a decimal anyways then I'm gonna truncate it.\n\n");
+        scanf("%s", str);
+        thenum = atoi(str);
+    }
     summed = sum(thenum);
     avged  = avg(thenum);
 
     printf("the num is %d\n",  thenum);
     printf("the sum is %d\n",  summed);
     printf("the avg is %lf\n", avged);
-
-    printf("and git works from home-desktop w/ WSL exactly as it does on the laptop now\n");
 
     return 0;
 }
