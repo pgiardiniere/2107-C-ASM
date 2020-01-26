5d56f9a686406b42f022d146262a0b59e97b9533
pgiardiniere <pgiardiniere@gmail.com>
2020-01-26 15:42:50 -0500

    reorg + lab02 initial commit

---
+++ 02-stringy/lab2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>          // may use no other libraries in this assignment
+
+int length(char str[]) {
+    int i = 0;
+    char c;
+    do { i++; } while (c != '\0');
+
+    return i;
+}
+
+int isLower(char c) {
+    return 0; // i.e. return false. 1 == true.
+}
+
+void toUpper(char c){
+    // in-place reassignment? seems like based on instructions
+}
+
+char getString(stdin) {
+    printf("Enter an input string\n");
+}
+
+void capitalizeAndCount(char str[]) {
+
+}
+
+void printToConsole() {
+
+}
+
+int main() {
+    char s[] = "poop";
+
+    s.length();
+
+
+    return 0;
+}
