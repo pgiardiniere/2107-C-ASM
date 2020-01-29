e0a0dcde363460ec03156ea7b841c6b8d0a05d58
pgiardiniere <pgiardiniere@gmail.com>
2020-01-29 14:11:11 -0500

    add toUpper, fix others

--- 02-stringy/lab2.c
+++ 02-stringy/lab2.c
@@ -1,42 +1,21 @@
-// notes: Am using 2 print funcs to different types.
+#include <stdio.h>
 
-#include <stdio.h>          // may use no other libraries in this assignment
+char str[] = "placeholder";
 
 int length(char str[]) {
     int i = 0;
-    char c;
-    do { i++; } while (c != '\0');
-
+    while (str[i] != '\0') { i++; }
     return i;
 }
 
 int isLower(char c) {    
-    char a = 'a';
-    char z = 'z';
-    // char A = 'A';
-    // char Z = 'Z';
-    // char zero = '0';
-    // char nine = '9';
-
-    // printf("a index is %d\n", a); // 97
-    // printf("z index is %d\n", z); // 122
-    // printf("A index is %d\n", A); // 65
-    // printf("Z index is %d\n", Z); // 90
-    // printf("0 index is %d\n", zero); // 48
-    // printf("9 index is %d\n", nine); // 57
-
-    // printInt(z-a); // diff is 25
-    // printInt(Z-A); // diff is 25
-    // printInt(a-A); // diff is 32 (imp)
-    // printInt(z-Z); // diff is 32 (imp)
-
-    if (c >= a && c <= z) return 1;
+    if (c >= 'a' && c <= 'z') return 1;
     else return 0;
 }
 
-void toUpper(char c){
-    
-    // in-place reassignment? seems like based on instructions
+char toUpper(char c){
+    c -= 32;
+    return c;
 }
 
 // // throws a warning for now, can do "return *s" to silence
@@ -54,15 +33,30 @@ void printInt(int i) { printf("%d\n", i); }
 void printString(char s[]) { printf("%s\n", s); }
 
 int main() {
-    char c = 'c';
-    char C = 'C';
-    char doi = '{';
-    printInt(isLower(c));
-    printInt(isLower(C));
-    printInt(isLower(doi));
+    // copy global var 'str' into 's' 
+    int len = length(str);
+    char s[len];
+    int i = 0;
+    while (str[i] != '\0') { 
+        s[i] = str[i];
+        i++;
+    }
+    s[len] = '\0';
 
-    char s[] = "poop";
-    int len = length(s);
+    // print each character (and position) to screen.
+    int j = 0;
+    while (j < len) {
+        printf("charat %d is %c\n", j, s[j]);
+        j++;
+    }
+
+    // test toUpper
+    printf("%c\n", toUpper('d'));
+
+    // test isLower
+    printf("%d\n", isLower('D'));
+    printf("%d\n", isLower('d'));
+    printf("%d\n", isLower('4'));
 
     return 0;
 }
