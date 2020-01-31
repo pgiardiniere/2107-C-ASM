51a4b3850d7e40af7d2eff86413f2a820c2766cb
pgiardiniere <pgiardiniere@gmail.com>
2020-01-31 15:22:59 -0500

    add comments, clean dir

--- 02-stringy/lab2.c
+++ 02-stringy/lab2.c
@@ -1,67 +1,76 @@
+// Coded by Pete Giardiniere
+// Includes 2 print functions. 
+    // printVertical() --- outputs per assignment specs exactly (currently called in main)
+    // print()         --- outputs in horizontal format
+
 #include <stdio.h>
 
-char str[] = "place";
-
-int length(char str[]) {
+int length (char str[]) {
     int i = 0;
-    while (str[i] != '\0') { i++; }
+    while (str[i] != '\n') { i++; }
     return i;
 }
 
-int isLower(char *c) {    
+int isLower(char *c) {
     if (*c >= 'a' && *c <= 'z') return 1;
     else return 0;
 }
 
-void toUpper(char c){ c -= 32; }
+void toUpper(char *c) {
+    if (*c >= 'a' && *c <= 'z') *c -= 32;
+}
 
-int capitalize(char *s[]) {
-    int count = 0;
-    int i = 0;
-    for (i = 0; i < length(*s); i++) {
-        if (isLower( (int) *s[i]) ) {
-            toUpper(*s[i]);
-            count++;
-        }
+void capitalize(char *c, int *changed) {
+    if (isLower(c)) {
+        toUpper(c);
+        *changed += 1;      // use += as ++ increments pointer pos, not the value stored in changed
     }
-    return count;
 }
 
-// // throws a warning for now, can do "return *s" to silence
-// char getString(char s[]) {
-//     printf("Enter an input string\n");
-
-//     return s;
-// }
-
-void capitalizeAndCount(char str[]) {
-
+// in-place reverse, avoids copying & pointer complications
+void reverse (char s[], int right) {
+    int left = 0;
+    char tmp;
+    while (left < right) {
+        tmp = s[left];
+        s[left] = s[right];
+        s[right] = tmp;
+        left++;
+        right--;
+    }
 }
 
-void printInt(int i) { printf("%d\n", i); }
-void printString(char s[]) { printf("%s\n", s); }
+void getString(char s[]) {
+    printf("Please input a string, max length 256 characters. Hit enter when finished\n");
+    fgets(s, 256, stdin);
+}
+
+void print(char s[], int len, int changed) {
+    printf("    String is : %s", s);
+    printf("    length is : %d\n", len);
+    printf("    Changed is: %d\n\n", changed);
+}
+
+void printVertical(char s[], int len, int changed) {
+    int i = 0;
+    for (i = 0; i < len; i++) 
+        printf("    %c\n", s[i]); 
+    printf("    length is : %d\n", len);
+    printf("    Changed is: %d\n\n", changed);    
+}
 
 int main() {
-    // copy global var 'str' into 's' 
-    int len = length(str);
-    char s[len];
+    char s[256];
+    getString(s);
+    int len = length(s);
+    int changed = 0;
+
+    printVertical(s, len, changed);
     int i = 0;
-    while (str[i] != '\0') { 
-        s[i] = str[i];
+    while (i < length(s)) {
+        capitalize(&s[i], &changed);
         i++;
     }
-    s[len] = '\0';
-
-    // print each character (and position) to screen.
-    int j = 0;
-    while (j < len) {
-        printf("charat %d is %c\n", j, s[j]);
-        j++;
-    }
-
-    // test
-    printf("capitlized chars: %d\n", capitalize(&s));
-    printf("%s\n", s);
-
-    return 0;
+    reverse(s, len-1);
+    printVertical(s, len, changed);
 }
