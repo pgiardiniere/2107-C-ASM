551b51cc4c3126ffa2bfbb6250bc36ef8ee391f4
pgiardiniere <pgiardiniere@gmail.com>
2020-02-10 19:12:31 -0500

    file opens successfully, passes contents

--- 03-OTP-encrypt/lab3.c
+++ 03-OTP-encrypt/lab3.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char * read_file(char *filename, unsigned long *size);
-void write_file(char *output, char *filename, unsigned long size);
+char * read_file(char *filename, unsigned long *filesize);
+void write_file(char *output, char *filename, unsigned long filesize);
 
 
-char * read_file(char *filename, unsigned long *size) {
-    
-    printf("%s, %d\n", filename, *size);
+char * read_file(char *filename, unsigned long *filesize) {
+    FILE * file = fopen(filename, "rb");
+    if (file == NULL) { return "File not found"; }
+
+
+
+
+    *filesize = 10 ;    // = fseek()
     return filename;
 }
 
-void write_file(char *output, char *filename, unsigned long size) {
+void write_file(char *output, char *filename, unsigned long filesize) {
 
 }
 
 
 int main() {
     char filename[] = "file.txt";
-    unsigned long filesize = 10;         // 10 bytes
+    unsigned long filesize = 0;
 
-    read_file(filename, &filesize);
+    char *filecontent = read_file(filename, &filesize);
+    if (strcmp(filecontent, "File not found") == 0) printf("%s\n", filecontent);    // works
 
-    printf("der\n");
+    printf("%d\n", filesize);
     return 0;
 }
