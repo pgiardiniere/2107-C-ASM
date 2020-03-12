8149f19faf12409902b49dd806165040a74ca457
pgiardiniere <pgiardiniere@gmail.com>
2020-03-12 13:43:16 -0400

    bits.c - working bit-copy

---
+++ 04-BMP-encrypt/bits.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void stringReverse(char * str) {
+    char temp[sizeof(str)];
+    int i = 0;
+    int j = 0;
+    while (str[i]) { i++; } i--;    // walk index to end of data
+
+    while (i > -1) {                // reverse in a temp array
+        temp[j] = str[i];
+        j++;
+        i--;
+    }
+    temp[j] = '\0';
+    strcpy(str, temp);              // overwrite original, reverse complete
+}
+
+int main() {
+    char binRep[16];
+    unsigned int n = 4;             // Binary : 0000 0000 0000 0100
+    int i = 0;
+
+    printf("base-10 n is : %d\n", n);
+    
+    while (n) {                     // get binary of n. Begins @ least significant (i.e. rightmost) digit.
+        if (n & 1)
+            binRep[i] = '1';
+        else
+            binRep[i] = '0';
+        i++;
+        n = n >> 1;
+    }
+    binRep[i] = '\0';
+    stringReverse(binRep);
+    
+    printf("base-2  n is : %s\n", binRep);
+    return 0;
+}
