4ed30cad9e3948f701c5adaeb40675d4409fe154
pgiardiniere <pgiardiniere@gmail.com>
2020-03-17 16:50:22 -0400

    add() and sub() over/under-flow detect correctly

--- 05-convertCalc/test-modules/add-sub.c
+++ 05-convertCalc/test-modules/add-sub.c
@@ -4,17 +4,19 @@
 
 int add (int a, int b) {
     int c = a + b;
-    if (a >= 0 && b >= 0 && c < a) { // do  a+b, if c<a, then we overflowed
+    if (a >= 0 && b >= 0 && c < a) {      // do  a+b, if c<a, then we overflowed
         printf("Overflow\n"); return(0);
     }
 
-    else if (a >= 0 && b <= 0 && c > a) { // do  a-b, if c>a, then we underflowed
-        printf("Underflow\n"); return(0);
-    }
+    // **********  This block is redundant  **********
+    // else if (a >= 0 && b <= 0 && c > a) { // do  a-b, if c>a, then we underflowed
+    //     printf("Underflow\n"); return(0);
+    // }
 
-    else if (a <= 0 && b >= 0 && c < a) { // do -a+b, if c<a, then we overflowed
-        printf("Overflow\n"); return(0);
-    }
+    // else if (a <= 0 && b >= 0 && c < a) { // do -a+b, if c<a, then we overflowed
+    //     printf("Overflow\n"); return(0);
+    // }
+    // ***********************************************
 
     else if (a <= 0 && b <= 0 && c > a) { // do -a-b, if c>a, then we underflowed
         printf("Underflow\n"); return(0);
@@ -27,37 +29,74 @@ int add (int a, int b) {
 }
 
 int sub (int a, int b) {
-    int c = a + b;
+    int c = a - b;
+    // Redundant :: pos - pos CAN'T underflow
+    // if (a >= 0 && b >= 0 && c < a) {
+    //     printf("Overflow\n"); return(0);
+    // }
 
+    if (a >= 0 && b <= 0 && c < a) {      // do  a+b, if c<a, then we overflowed
+        printf("Overflow\n"); return(0);
+    }
+
+    else if (a <= 0 && b >= 0 && c > a) { // do -a-b, if c>a, then we underflowed
+        printf("Underflow\n"); return(0);
+    }
+
+    // Redundant :: neg - neg CAN'T overflow
+    // else if (a <= 0 && b <= 0 && c > a) { // do -a+b, if c>a, then we underflowed
+    //     printf("Underflow\n"); return(0);
+    // }
+
+    else {
+        printf("Calculation is in-bounds. Result is %d\n", c);
+        return c;
+    }
+}
+
+void testAdd() {
+    int a; int b; int c;
+    // Case :: a Pos, b Pos
+    a = INT_MAX;
+    b = INT_MAX;
+    add(a, b);
+    // Case :: a Pos, b Neg
+    a = 0;
+    b = INT_MIN;
+    add(a, b);
+    // Case :: a Neg, b Pos
+    a = INT_MIN;
+    b = 0;
+    add(a, b);
+    // Case :: a Neg, b Neg
+    a = INT_MIN;
+    b = INT_MIN;
+    add(a, b);
+}
+
+void testSub() {
+    int a; int b; int c;
+    // Case :: a Pos, b Pos
+    a = INT_MAX;
+    b = INT_MAX;
+    sub(a, b);                  // Expect ::  0             Actual ::  0
+    // Case :: a Pos, b Neg
+    a = 0;
+    b = INT_MIN;
+    sub(a, b);                  // Expect :: Overflow       Actual :: Overflow
+    // Case :: a Neg, b Pos
+    a = INT_MIN;
+    b = 1;
+    sub(a, b);                  // Expect :: Underflow      Actual :: Underflow
+    // Case :: a Neg, b Neg
+    a = INT_MIN;
+    b = INT_MIN;
+    sub(a, b);                  // Expect :: 0              Actual :: 0
 }
 
 int main() {
-    // Testing :: Order of submissions
-    // a >= 0, b >= 0   (a Pos, b Pos)
-    // a >= 0, b <= 0   (a Pos, b Neg)
-    // a <= 0, b >= 0   (a Neg, b Pos)
-    // a <= 0, b <= 0   (a Neg, b Neg)
-    int a; int b;
-    
-    a = INT_MAX;
-    b = INT_MAX;
-    add(a, b);      // a + b  Should be  4 mil   Will overflow,   -(something)
-    // sub(a, b)    // a - b  Should be  0
-    
-    a = INT_MAX;    // a + b  Should be  -1
-    b = INT_MIN;    // a - b  Should be  4 mil   Will underflow,  -(something)
-    add(a, b);
-    // sub(a, b)
-
-    a = INT_MIN;    // a + b  Should be  -1
-    b = INT_MAX;    // a - b  Should be  -4 mil  Will underflow,  +(something)
-    add(a, b);
-    // sub(a, b)
-
-    a = INT_MIN;    // a + b  Should be   -4 mil  Will underflow, +(something)
-    b = INT_MIN;    // a - b  Should be   0
-    add(a, b);
-    // sub(a, b)
+    testAdd(); printf("\n");
+    testSub(); printf("\n");
 
     return 0;
 }
