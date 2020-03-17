72bca24c08a24d055b01a261b1be5442514333d7
pgiardiniere <pgiardiniere@gmail.com>
2020-03-16 22:15:13 -0400

    formatting

--- 05-convertCalc/safeCalcProb.c
+++ 05-convertCalc/safeCalcProb.c
@@ -253,14 +253,14 @@ int main(int argc, char *argv[]) {
     unsigned long size = sizeof(int);
     printf("sizeof int is %lu\n", size);
     printf("bitsof int is %lu\n", size*8);
-    printf("bit representation :: 32 bits");
-    printf("0000 0000 0000 0000 0000 0000 0000 0000");
-    printf("hex representation :: 32 bits");
-    printf("FFFF FFFF");                                //    implies we have 0x80000000        (calculator verifies this)
-    printf("Integer Min is %d\n", INT_MIN);             // define __INT_MIN__ ( -INT_MAX - 1)   == (decimal) -2,147,483,648
-    printf("Integer Max is %d\n", INT_MAX);             // define __INT_MAX__ 0x7fffffff        == (decimal)  2,147,483,647
-                                                        // MIN binary :: 1000 0000 0000 0000 0000 0000 0000 0000
-                                                        // MAX binary :: 0111 1111 1111 1111 1111 1111 1111 1111
+    printf("bit representation :: 32 bits\n");
+    printf("0000 0000 0000 0000 0000 0000 0000 0000\n");
+    printf("hex representation :: 32 bits\n");
+    printf("FFFF FFFF\n");                                                  // ^> implies we have 0x80000000        (Verified)
+    printf("Integer Min is %d with hexcode %x\n", INT_MIN, INT_MIN);        // define __INT_MIN__ ( -INT_MAX - 1)   == (decimal) -2,147,483,648
+    printf("Integer Max is  %d with hexcode %x\n", INT_MAX, INT_MAX);       // define __INT_MAX__ 0x7fffffff        == (decimal)  2,147,483,647
+                                                                            // MIN binary :: 1000 0000 0000 0000 0000 0000 0000 0000
+                                                                            // MAX binary :: 0111 1111 1111 1111 1111 1111 1111 1111
     exit(-1);
 
     // Loop until quit is selected
