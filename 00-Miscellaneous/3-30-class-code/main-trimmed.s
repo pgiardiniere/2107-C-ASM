76910ca63b05ee5d0be4fc82733e645359f4a555
pgiardiniere <pgiardiniere@gmail.com>
2020-03-31 02:43:54 -0400

    lecture 3-30 part 2, final

---
+++ 00-Miscellaneous/3-30-class-code/main-trimmed.s
@@ -0,0 +1,17 @@
+	.file	"main.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movl	$30, -12(%rbp)
+	movl	$57, -8(%rbp)
+	movl	$39, -4(%rbp)
+	movl	-12(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%eax, %edx
+	movl	-4(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	ret
