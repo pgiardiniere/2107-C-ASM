ad54096dbe63c5c014215348b8a8bdcbc558814d
pgiardiniere <pgiardiniere@gmail.com>
2020-04-05 20:01:20 -0400

    lab7 prob-2 remove extraneous asm complete

--- 07-asm/2-prob/between.s
+++ 07-asm/2-prob/between.s
@@ -4,12 +4,8 @@
 	.type	between, @function
 between:
 .LFB0:
-	.cfi_startproc
 	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
 	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
 	movl	%edi, -4(%rbp)
 	movl	%esi, -8(%rbp)
 	movl	%edx, -12(%rbp)
@@ -25,10 +21,4 @@ between:
 	movl	$0, %eax
 .L3:
 	popq	%rbp
-	.cfi_def_cfa 7, 8
 	ret
-	.cfi_endproc
-.LFE0:
-	.size	between, .-between
-	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04) 7.4.0"
-	.section	.note.GNU-stack,"",@progbits
