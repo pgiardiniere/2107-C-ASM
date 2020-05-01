fe065df85bc4dd46b37404fde361ea5c67a2b889
pgiardiniere <pgiardiniere@gmail.com>
2020-05-01 04:29:28 -0400

    working read(), partial functionality

--- 02-final/product.c
+++ 02-final/product.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define GROW_SIZE 2
 
@@ -22,68 +23,88 @@ ProductArray make_product_array(int length) {
         perror("error: ");
         exit(-1);
     }
-    ProductArray products = { arr, length, 0 };
-    return products;
+    ProductArray pl = { arr, length, 0 };
+    return pl;
 }
 
-void delete_product_array(ProductArray* products) {
-    if ((*products).arr == NULL) {
+void delete_product_array(ProductArray* pl) {
+    if ((*pl).arr == NULL) {
         return;
     }
-    free((*products).arr);
-    (*products).length = 0;
-    (*products).count = 0;
+    free((*pl).arr);
+    (*pl).length = 0;
+    (*pl).count = 0;
 }
 
-void insert_product(ProductArray* products, Product prod) {
+void insert_product(ProductArray* pl, Product prod) {
     // If full, double length and reallocate array according to length * sizeof struct.
-    if (products->count == products->length) {
-        (*products).length = GROW_SIZE * products->length;
-        (*products).arr = realloc((*products).arr, (*products).length * sizeof(Product));
+    if (pl->count == pl->length) {
+        (*pl).length = GROW_SIZE * pl->length;
+        (*pl).arr = realloc((*pl).arr, (*pl).length * sizeof(Product));
 
-        if ((*products).arr == NULL) {
+        if ((*pl).arr == NULL) {
             perror("error : ");
             exit(-1);
         }
     }
     // Now that we're certainly not full, append the thing.
-    (*products).arr[(*products).count] = prod;
-    (*products).count += 1;
+    (*pl).arr[(*pl).count] = prod;
+    (*pl).count += 1;
 }
 
 ProductArray* read(char* filename) {
+    // Declarations
+    ProductArray pl = make_product_array(GROW_SIZE);
+    char id[6];
+    char desc[50];
+    double price;
+    int qty;
+
     FILE* file = fopen(filename, "r");
     if (file == NULL) {
         perror("error : ");
         exit(-1);
     }
-    // Create a product array to store file contents.
-    ProductArray products = make_product_array(GROW_SIZE);
-    // Make product from file text: First, determine length of file
+    
+    // Determine length of file
     int j = 0;
-    char* line;
+    char* line = (char*) malloc(255 * sizeof(char));
+    if (line == NULL) { perror("error: "); exit(-1); }
     while (fgets(line, 255, file) != NULL) {
         j++;
     }
     rewind(file);
-    // Second, read each line, making a product from each.
+    
+    // Read file, making products 1 line at a time.
     int i = 0;
     for (i = 0; i < j; i++) {
         fgets(line, 255, file);
-        strtok(line, "\n");     // overwrite fgets trailing newline with null char
-        
-    }
+        strtok(line, "\n");
 
+        char* ptr = strtok(line, ",");
+        strncpy(id, ptr, 5);
+        id[5] = '\0';
+        
+        ptr = strtok(NULL, ",");
+        strcpy(desc, ptr);
+
+        ptr = strtok(NULL, ",");
+        price = atof(ptr);
+
+        ptr = strtok(NULL, ",");
+        qty = atoi(ptr);
+
+        Product p = { id, desc, price, qty };
+    }
 
     // Insert product into our product array.
 
+    free(line);
     fclose(file);
 }
 
 int main() {
-    printf("hi\n");
-
-    // read("Products.txt");
+    read("Products.csv");
 
     return 0;
 }
